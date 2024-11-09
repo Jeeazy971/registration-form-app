8b6d4aa8ed2a9bb43f5c6d045e425cbd024b5f73
Josué Bayidikila <josue-dev@outlook.fr>
2024-11-10 00:39:04 +0100

    change files

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
-  testEnvironment: "jsdom",
-  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
-};
